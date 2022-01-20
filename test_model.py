0f6e6c21a5a65658e459be034d49f7a91331ece8
Michael Egorov <michwill@gmail.com>
2022-01-20 10:31:25 +0100

    test p_top_bottom

--- test_model.py
+++ test_model.py
@@ -4,6 +4,10 @@
 from libmodel import LendingAMM
 
 
+def approx(x, y):
+    return 2 * abs(x - y) / abs(x + y)
+
+
 def test_p_up_down():
     amm = LendingAMM(p_base=1000, A=100)
 
@@ -12,3 +16,15 @@ def test_p_up_down():
         p_down = amm.p_down(i)
         assert p_up > p_down
         assert amm.p_down(i + 1) == p_up
+
+
+def test_p_top_bottom():
+    amm = LendingAMM(p_base=1000, A=100)
+
+    for i in range(-100, 100):
+        p = amm.p_top(i)
+        amm.p_oracle = p
+        assert approx(amm.p_down(i), p) < 1e-12
+        p = amm.p_bottom(i)
+        amm.p_oracle = p
+        assert approx(amm.p_up(i), p) < 1e-12
