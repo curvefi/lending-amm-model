66abf63b3771c8f95d607e0cfe0947ce89bc8566
Michael Egorov <michwill@gmail.com>
2022-01-25 19:55:30 +0100

    More scans

---
+++ model-T.py
@@ -0,0 +1,20 @@
+from libsimulate import get_loss_rate
+
+if __name__ == '__main__':
+    import numpy as np
+
+    range_size = 0.18
+    T = np.logspace(np.log10(200), np.log10(100000), 20)
+    losses = []
+    for t in T:
+        losses.append(get_loss_rate(range_size, 0.003, Texp=float(t), measure='xtopmax2', min_loan_duration=30, max_loan_duration=30, samples=500))
+        print(t, losses[-1])
+
+    try:
+        import matplotlib
+        matplotlib.use('tkagg')
+        import pylab
+        pylab.semilogx(T, losses)
+        pylab.show()
+    except ImportError:
+        pass
--- model-range_size.py
+++ model-range_size.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
     fee = 0.003
     losses = []
     for r in range_size:
-        losses.append(get_loss_rate(r, fee, measure='xtopmax2', min_loan_duration=30, max_loan_duration=30, samples=1000))
+        r = float(r)
+        losses.append(get_loss_rate(r, fee, Texp=10000, measure='xtopmax2', min_loan_duration=30, max_loan_duration=30, samples=1000))
         print(r, losses[-1])
 
     try:
