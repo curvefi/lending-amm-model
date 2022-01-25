66013a1398170a31eb418e166b570ca3d80cf800
Michael Egorov <michwill@gmail.com>
2022-01-25 20:07:59 +0100

    model-fee

--- model-fee.py
+++ model-fee.py
@@ -3,11 +3,12 @@ from libsimulate import get_loss_rate
 if __name__ == '__main__':
     import numpy as np
 
-    range_size = 0.18
-    fee = np.logspace(np.log10(0.0003), np.log10(0.1), 3)
+    range_size = 0.1
+    fee = np.logspace(np.log10(0.0003), np.log10(0.3), 7)
     losses = []
     for f in fee:
-        losses.append(get_loss_rate(range_size, f, measure='xavg', min_loan_duration=30, max_loan_duration=30, samples=1000))
+        f = float(f)
+        losses.append(get_loss_rate(range_size, f, Texp=10000, measure='xavg', min_loan_duration=30, max_loan_duration=30, samples=500))
         print(f, losses[-1])
 
     try:
