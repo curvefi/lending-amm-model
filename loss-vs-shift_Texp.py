a8c1c085e0725da7c022e92fc3353e072682b60b
Michael Egorov <michwill@gmail.com>
2022-05-24 01:17:53 +0200

    Loss vs Texp

---
+++ loss-vs-shift_Texp.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import matplotlib
+import pylab
+import numpy as np
+from libsimulate import get_loss_shift
+
+# TEXP = 10000
+LOAN_DURATION = 3  # days
+SAMPLES = 50000
+FEE = 0.01
+MIN_RANGE = -0.2
+N_BINS = 30
+RANGE_SIZE = 0.2
+
+matplotlib.use('TkAgg')
+
+for texp in [1000, 5000, 10000, 30000, 50000, 100000]:
+    results, shifts = get_loss_shift(RANGE_SIZE, FEE, Texp=texp, min_loan_duration=LOAN_DURATION, max_loan_duration=LOAN_DURATION, samples=SAMPLES)
+
+    results = np.array(results)
+    shifts = np.array(shifts)
+
+    bins = np.linspace(MIN_RANGE, max(shifts), N_BINS)
+    db = bins[1] - bins[0]
+
+    xshifts = [100 * shifts[(shifts >= s) * (shifts < s + db)].mean() for s in bins]
+    xloss = [100 * results[(shifts >= s) * (shifts < s + db)].mean() for s in bins]
+
+    pylab.plot(xshifts, xloss, label='Texp={0}'.format(texp))
+
+pylab.xlabel('Price drop relative to liquidation threshold [%]')
+pylab.ylabel('Relative loss [%]')
+pylab.title('Losses during a dip')
+pylab.legend()
+pylab.grid()
+pylab.show()
