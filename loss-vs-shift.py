44b88c55ed267edd6b897f2646aa2d0a4e4daeca
Michael Egorov <michwill@gmail.com>
2022-05-23 09:41:08 +0200

    Two graphs for comparison for loss(shift)

--- loss-vs-shift.py
+++ loss-vs-shift.py
@@ -4,26 +4,29 @@ import pylab
 import numpy as np
 from libsimulate import get_loss_shift
 
-RANGE_SIZE = 0.2
-FEE = 1e-2
+# RANGE_SIZE = 0.2
 TEXP = 10000
 LOAN_DURATION = 3  # days
 SAMPLES = 50000
+FEE = 0.01
 
-results, shifts = get_loss_shift(RANGE_SIZE, FEE, Texp=TEXP, min_loan_duration=LOAN_DURATION, max_loan_duration=LOAN_DURATION, samples=SAMPLES)
+for range_size in [0.2, 0.05]:
+    results, shifts = get_loss_shift(range_size, FEE, Texp=TEXP, min_loan_duration=LOAN_DURATION, max_loan_duration=LOAN_DURATION, samples=SAMPLES)
 
-results = np.array(results)
-shifts = np.array(shifts)
+    results = np.array(results)
+    shifts = np.array(shifts)
 
-bins = np.linspace(-0.4, max(shifts), 40)
-db = bins[1] - bins[0]
+    bins = np.linspace(-0.4, max(shifts), 40)
+    db = bins[1] - bins[0]
 
-xshifts = [100 * shifts[(shifts >= s) * (shifts < s + db)].mean() for s in bins]
-xloss = [100 * results[(shifts >= s) * (shifts < s + db)].mean() for s in bins]
+    xshifts = [100 * shifts[(shifts >= s) * (shifts < s + db)].mean() for s in bins]
+    xloss = [100 * results[(shifts >= s) * (shifts < s + db)].mean() for s in bins]
 
-pylab.plot(xshifts, xloss)
+    pylab.plot(xshifts, xloss, label='range_size={0:.2f}'.format(range_size))
+
 pylab.xlabel('Price drop relative to liquidation threshold [%]')
 pylab.ylabel('Relative loss [%]')
 pylab.title('Losses during a dip')
+pylab.legend()
 pylab.grid()
 pylab.show()
