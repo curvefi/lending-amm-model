83cb77c777264530c499ae6311e1f25b60705823
Michael Egorov <michwill@gmail.com>
2023-05-13 17:17:38 +0200

    Fee modelling script (using tkagg)

--- model-fee.py
+++ model-fee.py
@@ -6,20 +6,22 @@ if __name__ == '__main__':
     range_size = 0.039999
     T = 600
     measure = 'xtopmax2'
-    fee = np.logspace(np.log10(0.001), np.log10(0.03), 10)
+    fee = np.logspace(np.log10(0.001), np.log10(0.015), 20)
     losses = []
     for f in fee:
         f = float(f)
         losses.append(get_loss_rate(range_size, f, Texp=T, measure=measure, min_loan_duration=.3, max_loan_duration=.3,
                                     samples=200_000, n_top_samples=20,
-                                    other={'dynamic_fee_multiplier': 0, 'use_po_fee': 1, 'po_fee_delay': 2}))
+                                    other={'dynamic_fee_multiplier': 0, 'use_po_fee': 1, 'po_fee_delay': 1}))
         print(f, losses[-1])
 
     try:
         import sys
+        import matplotlib
         if 'PyPy' not in sys.version:
-            import matplotlib
             matplotlib.use('Qt5Agg')
+        else:
+            matplotlib.use('TkAgg')
         import pylab
         pylab.plot(fee, losses)
         pylab.xlabel('Fee')
